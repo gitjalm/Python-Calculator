1adade0fe6da2d72c37a450685c5096cf49870c4
gitjalm <jonathanalm08@gmail.com>
2025-02-17 13:45:42 +0100

    Laget en simpel sjekk om det er mellomrom
    Laget en simpel sjekk om det er mellomrom hvis ikke så printes det en error i konsollen.

--- main.py
+++ main.py
@@ -6,31 +6,34 @@ def main():
         equation_list = equation.split()
         if equation.lower() == "stop":
             break
-        if equation_list[1] == "+":
-            a = equation.split('+')
-            answer = int(a[0])+int(a[1])
-            print(answer)
-        elif equation_list[1] == "-":
-            a = equation.split('-')
-            answer = int(a[0])-int(a[1])
-            print(answer)
-        elif equation_list[1] == "*":
-            a = equation.split('*')
-            answer = int(a[0])*int(a[1])
-            print(answer)
-        elif equation_list[1] == "/":
-            a = equation.split('/')
-            answer = int(a[0])/int(a[1])
-            print(answer)
-        elif equation_list[1] == "!":
-            if int(equation[0]) <= 0:
-                print("Cannot get the square root of numbers that are 0 or less.")
-            else:
-                a = equation.split('!')
-                answer = sqrt(int(a[0]))
+        if " " in equation:
+            if equation_list[1] == "+":
+                a = equation.split('+')
+                answer = int(a[0])+int(a[1])
                 print(answer)
+            elif equation_list[1] == "-":
+                a = equation.split('-')
+                answer = int(a[0])-int(a[1])
+                print(answer)
+            elif equation_list[1] == "*":
+                a = equation.split('*')
+                answer = int(a[0])*int(a[1])
+                print(answer)
+            elif equation_list[1] == "/":
+                a = equation.split('/')
+                answer = int(a[0])/int(a[1])
+                print(answer)
+            elif equation_list[1] == "!":
+                if int(equation[0]) <= 0:
+                    print("Cannot get the square root of numbers that are 0 or less.")
+                else:
+                    a = equation.split('!')
+                    answer = sqrt(int(a[0]))
+                    print(answer)
+            else:
+                print("Cannot recognise this type or incorrect use of spaces.")
         else:
-            print("Cannot recognise this type.")
+            print("Remember to use correct spaces in between numbers and symbols.")
 
 if __name__ == "__main__":
     main()
