412ee2375a39bc3e3a2274b27df187792b18578b
gitjalm <jonathanalm08@gmail.com>
2025-02-18 10:22:14 +0100

    La til nye kommentarer

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from math import sqrt  #Importerer bare square root fra math biblioteket.
 import tkinter as tk
 
-equation = ""
-temp_equation = ""
+equation = ""   #Flyttet equation variabel så den kan bli brukt av alle funksjoner og elementer.
+temp_equation = ""  #Laget en temp_equation for midlertidlig lagring av infoen så resten ikke blir rørt.
 
 def main(equation):
     equation_list = equation.split()    #Splitter equation stringen opp i en liste så index 1 blir hvilken type regneregel som skal brukes.
@@ -35,29 +35,29 @@ def main(equation):
     else:
         print("Remember to use correct spaces in between numbers and symbols.")     #Feilmelding.
 
-def button_press(num):
+def button_press(num):  #Funksjonen som blir kalt når du trykker en av knappene som ikke er Backspace eller er lik.
     global temp_equation
-    temp_equation += str(num)
-    lbl.config(text=temp_equation)
+    temp_equation += str(num)   #Legger til det tegne som ble trykt på kalkulatoren
+    lbl.config(text=temp_equation)  #Oppdaterer teksten på kalkulatoren så du kan se hva du har skrevet allerede.
 
 def equal(info):
     global equation
     global temp_equation
-    equation = temp_equation
+    equation = temp_equation    
     main(equation)
 
-def clear():
+def clear():    #Funksjonen som blir kalt npr brukeren trykker backspace knappen.
     global temp_equation
-    temp_equation = " "
+    temp_equation = " "     #Fjerner alle tegn og tall fra temp_equation.
     lbl.config(text=temp_equation)
 
 root = tk.Tk()
 root.title("Calculator")
 
-lbl = tk.Label(root, text="0", height=3)
-lbl.grid(row=0, column=0)
+lbl = tk.Label(root, text="0", height=3)    #Viser tallene og tegnene som allerde har blitt skrevet inn i kalkulatoren.
+lbl.grid(row=0, column=0)   #Velger hvor i kalkulatorens grid den befinner seg i.
 
-btn = tk.Button(root, text="Backspace", command=lambda: clear(), height=4, width=8)
+btn = tk.Button(root, text="Backspace", command=lambda: clear(), height=4, width=8)     #Knapp for å fjerne all forrige data.
 btn.grid(row= 5, column=3)
 
 btn = tk.Button(root, text="/", command=lambda: button_press(" / "), height=4, width=8)
@@ -97,4 +97,4 @@ btn.grid(row= 2, column=1)
 btn = tk.Button(root, text="9", command=lambda: button_press("9"), height=4, width=8)
 btn.grid(row= 2, column=2)
 
-root.mainloop()
+root.mainloop()     #Kjører GUI i en loop.
