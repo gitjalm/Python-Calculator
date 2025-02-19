c492cf622505490e4f663f7682188800ede6707c
gitjalm <jonathanalm08@gmail.com>
2025-02-19 08:33:13 +0100

    Laget en history funksjon
    Laget en history funksjon, som sammensetter ligningen å svaret og sender de til en liste for lagring.

--- main.py
+++ main.py
@@ -3,31 +3,37 @@ import tkinter as tk
 
 equation = ""   #Flyttet equation variabel så den kan bli brukt av alle funksjoner og elementer.
 temp_equation = ""  #Laget en temp_equation for midlertidlig lagring av infoen så resten ikke blir rørt.
+history = []    #Lager en liste.
 
 def main(equation):
     equation_list = equation.split()    #Splitter equation stringen opp i en liste så index 1 blir hvilken type regneregel som skal brukes.
     if equation_list[1] == "+":     #Sjekker om index 1 er et plus tegn.
         a = equation.split('+')     #Lager en ny liste med tallene hver for seg.
         answer = int(a[0])+int(a[1])    #Regner ut å setter sammen svaret.
+        add_to_history(answer)
         lbl.config(text=answer)  #Printer svaret.
     elif equation_list[1] == "-":
         a = equation.split('-')
         answer = int(a[0])-int(a[1])
+        add_to_history(answer)
         lbl.config(text=answer)  #Printer svaret.
     elif equation_list[1] == "*":
         a = equation.split('*')
         answer = int(a[0])*int(a[1])
+        add_to_history(answer)
         lbl.config(text=answer)  #Printer svaret.
     elif equation_list[1] == "/":
         a = equation.split('/')
         answer = int(a[0])/int(a[1])
+        add_to_history(answer)
         lbl.config(text=answer)  #Printer svaret.
-    elif equation_list[1] == "!":
+    elif equation_list[1] == "√":
         if int(equation_list[0]) <= 0:   #Sjekker om tallet er lik eller mindre enn 0.
             print("Cannot get the square root of numbers that are 0 or less.")  #Printer en feilmelding til brukeren.
         else:
-            a = equation.split('!')
+            a = equation.split('√')
             answer = sqrt(int(a[0]))    #Bruker den importerte funksjonen sqrt til å få kvadratroten til tallet.
+            add_to_history(answer)
             lbl.config(text=answer)  #Printer svaret.
     else:
         print("Cannot recognise this type.")     #Feilmelding.
@@ -48,6 +54,11 @@ def clear():    #Funksjonen som blir kalt når brukeren trykker backspace knappe
     temp_equation = " "     #Fjerner alle tegn og tall fra temp_equation.
     lbl.config(text=temp_equation)
 
+def add_to_history(answer):
+    global equation
+    joined_answers = f"{equation} = {answer}"    #Setter sammen ligningen og svaret på en fin måte ved hjelp av en f streng.
+    history.append(joined_answers)   #Legger til det nye svaret i en liste.
+
 root = tk.Tk()
 root.title("Calculator")
 root.resizable(False, False)
@@ -59,7 +70,7 @@ frame.grid(row=0, column=0, sticky="ew", padx=5, pady=5, columnspan=4)
 lbl = tk.Label(frame, text="0", height=3, width=22, font=("Arial", 14), anchor="w")    #Viser tallene og tegnene som allerde har blitt skrevet inn i kalkulatoren.
 lbl.grid(row=0, column=0, columnspan=4, sticky="ew")   #Velger hvor i kalkulatorens grid den befinner seg i.
 
-btn = tk.Button(root, text="Backspace", command=lambda: clear(), height=4, width=8)     #Knapp for å fjerne all forrige data.
+btn = tk.Button(root, text="⌫", command=lambda: clear(), height=4, width=8)     #Knapp for å fjerne all forrige data.
 btn.grid(row= 5, column=3)
 
 btn = tk.Button(root, text="/", command=lambda: button_press(" / "), height=4, width=8)
@@ -99,7 +110,7 @@ btn.grid(row= 2, column=1)
 btn = tk.Button(root, text="9", command=lambda: button_press("9"), height=4, width=8)
 btn.grid(row= 2, column=2)
 
-btn = tk.Button(root, text="Kvadratrot", command=lambda: button_press(" ! "), height=4, width=8)
+btn = tk.Button(root, text="√", command=lambda: button_press(" √ "), height=4, width=8)
 btn.grid(row= 1, column=3)
 
 root.mainloop()     #Kjører GUI i en loop.
